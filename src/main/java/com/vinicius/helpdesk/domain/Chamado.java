b93fc7dc25b62b2df96e1decef33146947a0ea4c
Vinícius Diniz <105671764+ViniciusDX@users.noreply.github.com>
2023-07-14 13:27:39 -0300

    anotacoes
    Anotações JPA para classes de domínio

--- src/main/java/com/vinicius/helpdesk/domain/Chamado.java
+++ src/main/java/com/vinicius/helpdesk/domain/Chamado.java
@@ -1,29 +1,46 @@
 package com.vinicius.helpdesk.domain;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
 import com.vinicius.helpdesk.domain.enums.Prioridade;
 import com.vinicius.helpdesk.domain.enums.Status;
+import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
+import java.io.Serializable;
 import java.time.LocalDate;
 
+@Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
 @EqualsAndHashCode(of = "id")
-public class Chamado {
+public class Chamado implements Serializable {
+    private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
+
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataAbertura = LocalDate.now();
+    @JsonFormat(pattern = "dd/MM/yyyy")
     private LocalDate dataFechamento;
+
+
     private Prioridade prioridade;
     private Status status;
     private String titulo;
     private String observacoes;
 
+    @ManyToOne
+    @JoinColumn(name = "tecnico_id")
     private Tecnico tecnico;
+
+    @ManyToOne
+    @JoinColumn(name = "cliente_id")
     private Cliente cliente;
 
     public Chamado(Integer id, Prioridade prioridade, Status status, String titulo, String observacoes, Tecnico tecnico, Cliente cliente) {
--- src/main/java/com/vinicius/helpdesk/domain/Tecnico.java
+++ src/main/java/com/vinicius/helpdesk/domain/Tecnico.java
@@ -1,20 +1,30 @@
 package com.vinicius.helpdesk.domain;
 
+import com.vinicius.helpdesk.domain.enums.Perfil;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 import java.util.ArrayList;
 import java.util.List;
 
+@Entity
 @Data
 @EqualsAndHashCode(callSuper = true)
 public class Tecnico extends Pessoa {
+    private static final long serialVersionUID = 1L;
+
+    @OneToMany(mappedBy = "tecnico")
     private List<Chamado> chamados = new ArrayList<>();
 
     public Tecnico() {
+        super();
+        addPerfil(Perfil.CLIENTE);
     }
 
     public Tecnico(Integer id, String nome, String cpf, String email, String senha) {
         super(id, nome, cpf, email, senha);
+        addPerfil(Perfil.CLIENTE);
     }
 }
