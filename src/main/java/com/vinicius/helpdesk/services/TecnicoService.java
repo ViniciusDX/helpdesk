8300c1d74256e8dd828422597953fdfcb6e5c171
Vinícius Diniz <105671764+ViniciusDX@users.noreply.github.com>
2023-07-16 00:30:00 -0300

    endpoint
    findAll para Tecnico

--- src/main/java/com/vinicius/helpdesk/services/TecnicoService.java
+++ src/main/java/com/vinicius/helpdesk/services/TecnicoService.java
@@ -6,6 +6,7 @@ import com.vinicius.helpdesk.services.exception.ObjectnotFoundException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
@@ -15,5 +16,9 @@ public class TecnicoService {
     public Tecnico findById(Integer id){
         Optional<Tecnico> obj = repository.findById(id);
         return obj.orElseThrow(() -> new ObjectnotFoundException("Objeto não encontrado! Id: " + id));
+    }
+
+    public List<Tecnico> findAll() {
+        return repository.findAll();
     }
 }
